4fae47ffc6fd78ea49d98da43006b0641b82db50
Kyle Smith [KDOR] <Kyle.Smith@kdor.ks.gov>
2023-03-14 11:15:21 -0500

    made change to index

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Midterm</title>
 </head>
-<h1>Kyle Smith INF653 Midterm Project</h1>
+<h1>Kyle Smith-INF653-Midterm Project</h1>
 <body>
 </body>
 </html>
